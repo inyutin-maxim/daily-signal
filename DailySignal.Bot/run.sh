bd5413fd1d4f26159a464f2557244de2a13ec0eb
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2019-07-22 23:15:43 +0300

    Update run.sh

--- DailySignal.Bot/run.sh
+++ DailySignal.Bot/run.sh
@@ -1,10 +1,12 @@
 dotnet run shares/TQTF/FXUS -1
 dotnet run shares/TQTF/FXIT -0.8
-dotnet run shares/TQTF/FXTB -0.3
+dotnet run shares/TQTF/FXTB -0.3 0.6
 
 dotnet run index/RTSI/RUABICP -0.3
 
 dotnet run shares/TQTF/FXGD -0.8 1.8
 dotnet run shares/TQTF/FXRL -0.6 2.1
 dotnet run shares/TQTF/FXCN -1.4 2.2
+dotnet run shares/TQTF/FXDE -1.0 2.1
+dotnet run shares/TQTF/FXUK -1.6 1.7
 dotnet run shares/TQBR/YNDX -1.0 1.1
